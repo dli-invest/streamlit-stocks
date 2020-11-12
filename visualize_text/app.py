885813aaeff827343d7c71e19dd90922ad45e0c3
David Li <davidli012345@gmail.com>
2020-11-12 02:59:02 +0000

    visualize ner

--- visualize_text/app.py
+++ visualize_text/app.py
@@ -10,12 +10,10 @@ python -m spacy download en_core_web_md
 # Need to add streamlit app
 import spacy
 import streamlit as st
-from spacy_streamlit import visualize_tokens, visualize_textcat
+from spacy_streamlit import visualize_ner
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_md")
 
 txt = st.text_area('Text to analyze', 'Input Text Goes Here')
 doc = nlp(txt)
-visualize_tokens(doc, attrs=["text", "pos_", "dep_", "ent_type_"])
-
-visualize_textcat(doc)
+visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
