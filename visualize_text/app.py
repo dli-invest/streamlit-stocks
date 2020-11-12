f93e6c7310ce79b029e464c60a7aa0e6a4987031
David Li <davidli012345@gmail.com>
2020-11-12 02:57:43 +0000

    visualizing textcat

--- visualize_text/app.py
+++ visualize_text/app.py
@@ -10,10 +10,12 @@ python -m spacy download en_core_web_md
 # Need to add streamlit app
 import spacy
 import streamlit as st
-from spacy_streamlit import visualize_tokens
+from spacy_streamlit import visualize_tokens, visualize_textcat
 
 nlp = spacy.load("en_core_web_sm")
 
 txt = st.text_area('Text to analyze', 'Input Text Goes Here')
 doc = nlp(txt)
 visualize_tokens(doc, attrs=["text", "pos_", "dep_", "ent_type_"])
+
+visualize_textcat(doc)
