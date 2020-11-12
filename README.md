b6abbb93a71ee30ddab42857a9d91221a1b8b906
David Li <davidli012345@gmail.com>
2020-11-12 02:41:10 +0000

    make sure text area works

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # streamlit-stocks
 Streamlit apps to visualize stocks
 
+To install
+```
 python3.8 -m pip install spacy-streamlit
+```
+
+Find the spacy models from https://github.com/explosion/spacy-models/releases
+
--- visualize_text/app.py
+++ visualize_text/app.py
@@ -6,9 +6,14 @@ Prerequisites:
 python -m spacy download en_core_web_sm
 python -m spacy download en_core_web_md
 """
+
+# Need to add streamlit app
 import spacy
+import streamlit as st
 from spacy_streamlit import visualize_tokens
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("This is a text")
+
+txt = st.text_area('Text to analyze', 'Input Text Goes Here')
+doc = nlp(txt)
 visualize_tokens(doc, attrs=["text", "pos_", "dep_", "ent_type_"])
