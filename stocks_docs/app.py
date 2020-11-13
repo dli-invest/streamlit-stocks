a1c6f28e39439ffa707f698af811cdd2904cf525
David Li <davidli012345@gmail.com>
2020-11-13 05:30:26 +0000

    fixing text

--- stocks_docs/app.py
+++ stocks_docs/app.py
@@ -17,8 +17,8 @@ st.sidebar.header('User Input Parameters')
 
 today = datetime.date.today()
 def user_input_features():
-    ticker = st.sidebar.text_input("Ticker", 'IP.CN')
-    webticker = st.sidebar.text_input("Ticker", 'IP.CN')
+    ticker = st.sidebar.text_input("Ticker", 'PKK.CN')
+    webticker = st.sidebar.text_input("Web Ticker", 'PKK.CNX')
     start_date = st.sidebar.text_input("Start Date", '2019-01-01')
     end_date = st.sidebar.text_input("End Date", f'{today}')
     return ticker, webticker, start_date, end_date
