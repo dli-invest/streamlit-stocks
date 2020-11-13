043f7c6a57debacf90ecf2ec23ed3c0979b26f2d
David Li <davidli012345@gmail.com>
2020-11-13 05:33:10 +0000

    lousy streamlit app

--- stocks_docs/app.py
+++ stocks_docs/app.py
@@ -19,7 +19,7 @@ today = datetime.date.today()
 def user_input_features():
     ticker = st.sidebar.text_input("Ticker", 'PKK.CN')
     webticker = st.sidebar.text_input("Web Ticker", 'PKK.CNX')
-    start_date = st.sidebar.text_input("Start Date", '2019-01-01')
+    start_date = st.sidebar.text_input("Start Date", '2014-01-01')
     end_date = st.sidebar.text_input("End Date", f'{today}')
     return ticker, webticker, start_date, end_date
 
